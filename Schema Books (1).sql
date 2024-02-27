1b867711b2b0f65c1fa7ec12fec460d985c1ea84
RubenCirelli <145332442+RubenCirelli@users.noreply.github.com>
2024-02-27 16:34:54 +0100

    Add files via upload

---
+++ Schema Books (1).sql
@@ -0,0 +1,10 @@
+CREATE TABLE Books(
+  book_id int , 
+  title text, 
+  genre text, 
+  author text, 
+  pubilshed_year int, 
+  isbn int, 
+  price int, 
+  rating int, 
+  stock_count int, publisher text, pages int)
