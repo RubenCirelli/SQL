c25e2eea6d4fb78d51719e499831fa7cd2955db5
Ruben <rubencirelli4@gmail.com>
2024-02-28 12:10:14 +0100

    Update

--- SQLite.sql
+++ SQLite.sql
@@ -3,3 +3,5 @@ INSERT INTO Books (book_id, title, genre, author, pubilshed_year, isbn, price, r
  (2, "War and Peacer", "Historical", "Leo Tolstoy", 1869, 1234567890124, 14.99, 4.8,  5, "Tolstoy Prints", 1200),
 (3, "Whispers of the Wind", "Romance", "Amelia Blackburn", 1982, 1234567890125, 9.99,  4.2, 20, " Blackburn House", 275),
 (4, "The Galactic Odyssey", "Science Fiction", "Orion Starfield", 2005, 1234567890126, 19.99, 4.9, 15, "Nebula Press", 450);
+
+UPDATE Books set stock_count=4 WHERE title='War and Peacer'
