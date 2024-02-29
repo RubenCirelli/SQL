f4fd5cd2129c2b93ba3d9a91373669deed383385
RubenCirelli <145332442+RubenCirelli@users.noreply.github.com>
2024-02-29 15:55:57 +0100

    Update Schema Books.sql

--- Schema Books.sql
+++ Schema Books.sql
@@ -1,10 +1,3 @@
-CREATE TABLE Books(
-  book_id int , 
-  title text, 
-  genre text, 
-  author text, 
-  pubilshed_year int, 
-  isbn int, 
-  price int, 
-  rating int, 
-  stock_count int)
+GRANT SELECT, UPDATE ON Books to  'martin'@'localhost';
+
+REVOKE UPDATE ON Books TO 'martin'@'localhost';
