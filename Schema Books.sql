aca9406bda505e2aa1e4b91fe9fb4524577de794
RubenCirelli <145332442+RubenCirelli@users.noreply.github.com>
2024-02-29 16:15:16 +0100

    Update Schema Books.sql

--- Schema Books.sql
+++ Schema Books.sql
@@ -1,10 +1,8 @@
-CREATE TABLE Books(
-  book_id int , 
-  title text, 
-  genre text, 
-  author text, 
-  pubilshed_year int, 
-  isbn int, 
-  price int, 
-  rating int, 
-  stock_count int)
+INSERT INTO Books (book_id, title, genre, author, pubilshed_year, isbn, price, rating, stock_count, publisher, pages)
+ VALUES (103," Journey to the Center", "Adventure", "Jules Verne", 1864, 1234567890123, 12.99, 4.5, 10, "Verne Publishing", 350);
+ 
+ BEGIN TRANSACTION; -- Inizia una nuova transazione
+
+DELETE FROM Books WHERE book_id=103;
+
+ROLLBACK TRANSACTION;
